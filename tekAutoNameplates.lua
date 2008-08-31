d1ebfcf1b0ce5d2298ed25f0aefb6d799e6c4258
Tekkub <tekkub@gmail.com>
2008-08-31 17:03:35 -0600

    Wrath compatibility

--- tekAutoNameplates.lua
+++ tekAutoNameplates.lua
@@ -1,5 +1,11 @@
+if IS_WRATH_BUILD == nil then IS_WRATH_BUILD = (select(4, GetBuildInfo()) >= 30000) end
+
 
 local f = CreateFrame("Frame")
 f:RegisterEvent("PLAYER_REGEN_DISABLED")
 f:RegisterEvent("PLAYER_REGEN_ENABLED")
-f:SetScript("OnEvent", function(self, event) if event == "PLAYER_REGEN_DISABLED" then ShowNameplates() else HideNameplates() end end)
+f:SetScript("OnEvent", function(self, event)
+	if IS_WRATH_BUILD then SetCVar("nameplateShowEnemies", event == "PLAYER_REGEN_DISABLED" and 1 or 0)
+	elseif event == "PLAYER_REGEN_DISABLED" then ShowNameplates()
+	else HideNameplates() end
+end)
